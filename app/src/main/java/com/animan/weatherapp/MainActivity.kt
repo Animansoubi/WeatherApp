ee062eef1427a7a493b3ecb097fa9e7e63956feb
Ani <anahidmansoubi2015@gmail.com>
2022-08-15 18:42:51 -0400

    Add menu to main activity

--- app/src/main/java/com/animan/weatherapp/MainActivity.kt
+++ app/src/main/java/com/animan/weatherapp/MainActivity.kt
@@ -2,7 +2,9 @@ package com.animan.weatherapp
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuCompat
 import com.animan.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+
+
+        binding.imgMenu.setOnClickListener {
+            binding.drawerLayout.openDrawer(binding.navigationView)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.drawer_menu, menu)
+        return true
     }
 }
